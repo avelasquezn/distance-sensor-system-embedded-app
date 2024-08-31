56e64578eb6bd848b8b993ed6b499e3ef2d9c90f
Angel Velasquez <pcisavel@upc.edu.pe>
2024-08-31 12:54:03 -0500

    chore: initial commit.

---
+++ DistanceSensor.h
@@ -0,0 +1,16 @@
+#ifndef DistanceSensor_h
+#define DistanceSensor_h
+
+#include "Arduino.h"
+
+class DistanceSensor {
+  public:
+    DistanceSensor(int assignedTriggerPin, int assignedEchoPin);
+    long readUltrasonicDistance();
+    float readMetricSystemDistance();
+  private:
+    int _triggerPin;
+    int _echoPin;
+};
+
+#endif
